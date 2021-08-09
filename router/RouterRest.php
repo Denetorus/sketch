65ba78d4c1b95d3c3f3271f61fd32c993c53f3dc
denetorus <gfc-com@mail.ru>
2021-08-09 20:24:30 +0300

    delete no vander

--- router/RouterRest.php
+++ router/RouterRest.php
@@ -1,20 +1,57 @@
 <?php
 
+namespace sketch\router;
 
-namespace router;
+use sketch\SK;
+use sketch\router\RouterBase;
 
-class RouterRest extends \sketch\router\RouterRest
+
+class RouterRest extends RouterBase
 {
 
-    public function routesAvailableWithoutSignIn(){
-        return [
+    public function run($signParams=[])
+    {
+        $uri = $this->getUri();
 
-            'test' => [
-                'path' => 'test',
-                'status' => -1
-            ],
+        $AvailablePath = $this->PathAvailableWithoutSignIn($uri);
 
+        if ( $AvailablePath===""){
+            return "";
+        }
 
-        ];
+        $uri = $AvailablePath;
+
+        $controller_path = SK::$controllers_path;
+        foreach ($this->routes() as $uriPattern => $path) {
+            if (preg_match("~$uriPattern~", $uri)) {
+
+                $internalRoute = preg_replace("~$uriPattern~", $path, $uri);
+                $parameters = explode('/', $internalRoute);
+                $controllerName = ucfirst(array_shift($parameters)).'Controller';
+
+                $controllerFile = CONTROLLER ."/". $controller_path ."/". $controllerName . '.php';
+                if (! file_exists($controllerFile)) {
+                    break;
+                }
+
+                include_once($controllerFile);
+
+                $actionName = "action".$_SERVER["REQUEST_METHOD"];
+
+                $className = "controller\\".$controller_path.'\\'.$controllerName;
+                $controllerObject = new $className;
+
+                $result = call_user_func_array(array($controllerObject, $actionName), $parameters);
+
+                if ($result === null) {
+                    break;
+                }
+
+                echo json_encode($result);
+
+                break;
+            }
+        }
+
     }
 }
