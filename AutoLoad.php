ec3af7351f45c0eb0fcc4a60601ca49999b998e6
denetorus <gfc-com@mail.ru>
2021-08-09 20:48:42 +0300

    composer test finish

--- AutoLoad.php
+++ AutoLoad.php
@@ -20,7 +20,7 @@ function AutoLoad($className)
         }
     }
     if (!$found) {
-        throw new Exception('There is no class file to download_: ' . $path );
+        throw new Exception('There is no class file to download: ' . $path );
     }
     return true;
 }
